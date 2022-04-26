b4cace93c3bae5d4e23eb643f092e433a57099c7
A90G <104040612+A90G@users.noreply.github.com>
2022-04-25 22:33:56 -0300

    Commit25/04

---
+++ Ejercicios/algoritmoDescuento.ts
@@ -0,0 +1,7 @@
+let precioInicial: number = ("450.50");
+let descuento: number = ("0.1");
+let precioFinal: number = (precioInicial - precioInicial * descuento);
+
+
+console.log(precioFinal);
+
---
+++ Ejercicios/receta.ts
@@ -0,0 +1,17 @@
+//Ingredientes receta
+let ingrediente1: string = "bananas";
+let ingrediente2: string = "manteca";
+let ingrediente3: string = "azùcar";
+let ingrediente4: string = "huevos";
+let ingrediente5: string = "harina integral";
+
+//Cantidades adecuadas
+let cantidadIngrediente1: string = "2 unidades";
+let cantidadIngrediente2: string = "100 gramos";
+let cantidadIngrediente3: string = "200 gramos";
+let cantidadIngrediente4: string = "2 unidades";
+let cantidadIngrediente5: string = "250 gramos";
+
+//Pasos de la receta
+console.log("Receta fácil para hacer muffins de ") + ingrediente1;
+console.log("");
